c94a6d611cb9204e9668fa10310ccb83ccdc03ae
SantiCarreno9 <santiago.carreno05@gmail.com>
2024-03-27 14:54:47 -0400

    Add project files.

---
+++ Source/Cpp_Group_Project/Cpp_Group_Project.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Cpp_Group_Project.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Cpp_Group_Project, "Cpp_Group_Project" );
